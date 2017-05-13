e3aca98239578917b374af20241642733c7158a3
Ruchita <ruchita@unlearner.com>
2017-05-13 11:34:15 -0700

    Add files via upload

---
+++ exercises/ex3.py
@@ -0,0 +1,12 @@
+print "I will now count my chciens:"
+print "hens", 25+30/6
+print "Roosters", 100-25 * 3 % 4
+print "Is it true taht 3+2 <5-8?"
+print 3+2 < 5 -7
+print "What is 3_2", 3+2
+print "What is 5-7",5-7
+print "oh, that's why it's False."
+print "how about some more."
+print "is it greater?", 5 > -2
+print "Is it greater or equal?", 5>= -2
+print "is it less or equal?", 5 <= -2
